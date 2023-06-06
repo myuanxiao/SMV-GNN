e1670afab3860d4e2cbb645ed36f63f45dae2cad
khmao <maokaihang@gmail.com>
2023-06-06 12:32:33 +0800

    baseline files

--- baseline/mole/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import deepchem as dc
-featurizer = dc.feat.WeaveFeaturizer()
-X = featurizer(["C", "CC"])
-y = np.array([1, 0])
-dataset = dc.data.NumpyDataset(X, y)
-model = dc.models.WeaveModel(n_tasks=1, n_weave=2, fully_connected_layer_sizes=[2000, 1000], mode="classification")
-loss = model.fit(dataset)
